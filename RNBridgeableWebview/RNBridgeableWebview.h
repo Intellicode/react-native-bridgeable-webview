da460387d4976da3655331bd466a25d6b035d37b
Tom Hastjarjanto <tom@intellicode.nl>
2015-06-18 21:24:29 +0200

    Add xcode project

---
+++ RNBridgeableWebview/RNBridgeableWebview.h
@@ -0,0 +1,13 @@
+//
+//  RNBridgeableWebview.h
+//  RNBridgeableWebview
+//
+//  Created by Tom on 6/18/15.
+//  Copyright (c) 2015 Tom Hastjarjanto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNBridgeableWebview : NSObject
+
+@end
---
+++ RNBridgeableWebview/RNBridgeableWebview.m
@@ -0,0 +1,13 @@
+//
+//  RNBridgeableWebview.m
+//  RNBridgeableWebview
+//
+//  Created by Tom on 6/18/15.
+//  Copyright (c) 2015 Tom Hastjarjanto. All rights reserved.
+//
+
+#import "RNBridgeableWebview.h"
+
+@implementation RNBridgeableWebview
+
+@end
