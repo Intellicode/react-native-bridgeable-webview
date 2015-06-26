cf118db581f0f398d8adf2085d25b21cd46a41b7
Tom Hastjarjanto <tom@intellicode.nl>
2015-06-26 23:41:46 +0200

    Fix @providesModule conflict

--- BridgeableWebView.ios.js
+++ BridgeableWebView.ios.js
@@ -5,8 +5,6 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
- *
- * @providesModule WebView
  * @flow
  */
 'use strict';
@@ -256,5 +254,5 @@ var styles = StyleSheet.create({
     backgroundColor: '#ffffff',
   }
 });
-
+dsadsadsadas@$@#@
 module.exports = WebView;
